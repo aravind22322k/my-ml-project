0c74db732121c12e860c266712281dc298af34b1
aravind22322k <aravindsg072@gmail.com>
2025-01-05 17:19:24 +0530

    Update train.py

--- data/scripts/train.py
+++ data/scripts/train.py
@@ -7,8 +7,8 @@ import pandas as pd
 
 def train_model(data_path, model_path):
     data = pd.read_csv(data_path)
-    X = data.drop("target", axis=1)
-    y = data["target"]
+    X = data.drop("class", axis=1)
+    y = data["class"]
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     
     model = RandomForestClassifier()
